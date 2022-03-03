292e5b9a0cbbce09562f6aa410905b8012a66190
Mitchel Pyl <mitchel.pyl+github@gmail.com>
2022-03-03 19:13:01 +0100

    Windows 10-ify window decorations

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ meson install -C _build
 gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'Main.loadTheme();'
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
 sleep 1
+gsettings set org.gnome.desktop.interface icon-theme 'Materia'
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
